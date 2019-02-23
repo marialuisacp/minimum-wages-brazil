8dc0fedc89a3d51d4a90f1a4c8770d01647ec933
Maria Luisa <marialuisaufmg@gmail.com>
2019-02-23 19:38:20 -0300

    chart and data fixed

--- src/chart.js
+++ src/chart.js
@@ -8,8 +8,11 @@ const parseYearDate = (year) => (
 );
 
 const convertToReal = (coin, value, year) => {
-  let newValue;
+  let newValue = value;
   switch (coin.toLowerCase()) {
+    case 'mil reis':
+      newValue = parseFloat(value) / 0.123;
+      break;
     case 'cr$':
       newValue = parseFloat(value) / Math.pow(1000, 5) * 2.75;
       break;
@@ -36,17 +39,24 @@ const convertToReal = (coin, value, year) => {
   return newValue;
 };
 
+const convertData = (d) => {
+  let stringDate = d.validity.replace(/\./g, '/'), parts = stringDate.split('/');
+  let day = parts[0], month = parts[1];
+
+  parts[0] = month;
+  parts[1] = day;
+  parts[2] = parseYearDate(parts[2]);
+
+  stringDate = parts.join('/');
+  d.year = parts[2];
+  d.date = new Date(stringDate);
+};
+
 const render = (data) => {
+
   data.
     forEach(d => {
-      let stringDate = d.validity.replace(/\./g, '/');
-      let parts = stringDate.split('/');
-      parts[2] = parseYearDate(parts[2]);
-
-      stringDate = parts.join('/');
-      d.year = parts[2];
-      d.date = new Date(stringDate);
-
+      convertData(d);
       d.realValue = convertToReal(d.coin, d.value, d.year);
     });
 
@@ -57,11 +67,11 @@ const render = (data) => {
 
   const size = {
     width: screen.width - margin.horizontal * 4,
-    height: 400
+    height: 500
   };
 
   const minDate = d3.min(data, (d) => (d.date));
-  const maxDate = d3.max(data, (d) => (d.date));
+  const maxDate = new Date();
 
   const x = d3.scaleTime()
     .domain([minDate, maxDate])
@@ -81,12 +91,20 @@ const render = (data) => {
     .ticks(10);
 
   const line = d3.line()
-    .x((d) => x(new Date(d.date)) || x(new Date()))
-    .y((d) => y(parseFloat(d.realValue)) || 0);
+    .x((d) => d.x)
+    .y((d) => d.y);
+
+  const defineAxis = (d) => {
+    d.x = x(new Date(d.date));
+    d.y = y(parseFloat(d.realValue));
+
+    if (d.y < 0)
+      d.y = d.y * -1;
+  };
+
+  data.forEach((d) => defineAxis(d));
 
   const renderNode = (d) => {
-    d.x = x(new Date(d.date)) || x(new Date());
-    d.y = y(parseFloat(d.realValue)) || 0;
     return 'translate(' + d.x + ',' + d.y + ')';
   };
 
@@ -120,20 +138,20 @@ const render = (data) => {
     .style('text-anchor', 'end')
     .text('R$');
 
+  svg.append('path')
+    .attr('class', 'line')
+    .attr('d', line(data));
+
   const nodes = svg.selectAll('.node')
     .data(data)
     .enter().append('g')
     .attr('class', 'node')
     .attr('transform', (d) => renderNode(d));
 
-  svg.append('path')
-    .attr('class', 'line')
-    .attr('d', line(data));
-
   nodes.append('circle')
     .attr('id', (d) => d.id)
     .attr('r', (d) => 3)
-    .style('fill', (d) => '#f00')
+    .attr('class', 'dot');
 };
 
 d3.json(pathData).then(render);
